0bfc86bb9b5f8f93ca987c5cde779ab5be87694c
Ricardo <Ricardo@RICARDO>
2021-09-03 14:06:56 +0200

    Initial Structure

---
+++ ScheduleNetCore.Api.Application.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScheduleNetCore.Api.Application.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ScheduleNetCore.Api.CrossCutting/README.txt
@@ -0,0 +1,4 @@
+﻿ricardoschedule@gmail.com
+petitsheldomlunaangie!#2021
+
+https://www.xvideos.com/video42218739/entrevista_de_sexo_familiar_con_ejemplos
